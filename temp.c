6bb926e9f697bcd652fb1c3fd8178b73bd7cd3fb
INTRON-26 <pvdheerajreddy@gmail.com>
2023-09-15 21:24:45 +0530

    Updates to links

---
+++ temp.c
@@ -0,0 +1,111 @@
+\documentclass{resume} % Use the custom resume.cls style
+
+\usepackage[left=0.4 in,top=0.4in,right=0.4 in,bottom=0.4in]{geometry} % Document margins
+\newcommand{\tab}[1]{\hspace{.2667\textwidth}\rlap{#1}}
+\newcommand{\itab}[1]{\hspace{0em}\rlap{#1}}
+\name{Pittala Venkata Dheeraj Reddy} % Your name
+% You can merge both of these into a single line, if you do not have a website.
+\address{+91 8008491559 \\ Hyderabad, India}
+\address{\href{mailto:pvdheerajreddy@gmail.com}{pvdheerajreddy@gmail.com} \\ \href{https://www.linkedin.com/in/venkata-dheeraj-reddy-pittala-b84baa214/}{Linkedin} \\ \href{https://github.com/INTRON-26}{Github} \\ \href{https://www.linkedin.com/in/venkata-dheeraj-reddy-pittala-b84baa214/}{Portfolio} \\ \href{https://leetcode.com/pvdheerajreddy/}{LeetCode} \\ \href{https://www.hackerrank.com/pvdheerajreddy}{HackerRank}}  %
+
+\begin{document}
+
+%----------------------------------------------------------------------------------------
+%	OBJECTIVE
+%----------------------------------------------------------------------------------------
+
+\begin{rSection}{OBJECTIVE}
+
+{Computer Science Student, looking for opportunities to work and contribute my skills with organizations.}
+
+\end{rSection}
+%----------------------------------------------------------------------------------------
+%	EDUCATION SECTION
+%----------------------------------------------------------------------------------------
+
+\begin{rSection}{Education}
+
+{\bf BTech in \textit{Computer Science and Engineering}  }\hfill {2020 - current}\\
+\textit{Gokaraju Rangaraju Institute of Engineering and Technology, Hyderabad}  \hfill { \textbf{9.35 CGPA} }\\
+ {\small  \textbf{Coursework:} Machine Learning, Software Engineering, Cloud Computing, Computer Networks, Artificial Intelligence, Data Science,
+Data Warehousing and Data Mining, Operating Systems, Database Management Systems, Data Structures and Algorithms}
+
+%Minor in Linguistics \smallskip \\
+%Member of Eta Kappa Nu \\
+%Member of Upsilon Pi Epsilon \\
+
+\end{rSection}
+
+%----------------------------------------------------------------------------------------
+% TECHINICAL STRENGTHS
+%----------------------------------------------------------------------------------------
+
+%----------------------------------------------------------------------------------------
+%	WORK EXPERIENCE SECTION
+\begin{rSection}{EXPERIENCE}
+
+\textbf{AWS Cloud Virtual Internship} \hfill Oct 2021 - Dec 2021\\
+AWS Academy | AICTE \hfill \textit{Hyderabad, India}
+ \begin{itemize}
+    \itemsep -3pt {}
+     \item Learnt various Cloud models, Cloud Architecture and services like EC2, S3, DynamoDB and Security
+     \item Learnt about AWS IAM, AWS auto scaling and different cost pricing in cloud.
+ \end{itemize}
+
+\end{rSection}
+%----------------------------------------------------------------------------------------
+
+\begin{rSection}{PROJECTS}
+\vspace{-1.25em}
+\item \textbf{Handwritten expression solver } \\
+\textit{Tools:} python, tkinter, wolframalpha, opencv \\
+{\textit{Overview:}  A Machile Learning project that solves the handwritten expression images
+\begin{itemize}
+    \item Used tkinter library to build a user interface
+    \item Developed modules to evaluate linear and higher order expressions.
+\end{itemize}
+}
+
+\item \textbf{Movie Recommendation System} \\
+{
+\textit{Tools:} python3, streamlit, pickle\\
+\textit{Overview:} A ML based project that recommends you a set of movies similar to your movie of choice.
+\begin{itemize}
+    \item Built a cosine similarity-based recommendation model in google colab
+    \item Developed the front-end part using streamlit library in python3.
+\end{itemize}
+}
+\item \textbf{Inventory Management  App} \\
+\textit{Tools:} React, Express, Node JS, Postman, Nodemon, Mongoose\\
+{\textit{Overview:}  A Full Stack Web Application that provides Inventory Analytics and Summary to the Registered Users
+\begin{itemize}
+    \item Used NodeJS and Express for Backend and React for Frontend.
+    \item User Authentication and Password Encryption are integrated to the application.
+\end{itemize}
+}
+
+\end{rSection}
+
+%----------------------------------------------------------------------------------------
+\begin{rSection}{Certifications}
+{\bf  The Complete 2023 Web Development Bootcamp}  \hfill { Udemy } \\
+{\bf AWS Cloud Virtual Internship  }  \hfill { Eduskills }
+\end{rSection}
+
+%----------------------------------------------------------------------------------------
+
+\begin{rSection}{SKILLS}
+
+\begin{tabular}{ @{} >{\bfseries}l @{\hspace{6ex}} l }
+Programming & Python, NodeJs, React, Express, MongoDB, Mongoose, Java, C, C++, HTML/CSS, JS, SQL
+\\
+Miscellaneous & Postman, Microsoft Office, Firebase, Git\\
+Soft Skills & Problem-solving, Time Management, Teamwork, Documentation,  Presentation
+\end{tabular}
+\end{rSection}
+
+\begin{rSection}{Languages}
+{English, Telugu, Hindi}
+\end{rSection}
+
+\end{document}
